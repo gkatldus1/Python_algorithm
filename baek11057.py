ad0de047ddec1a192c2c90a280cd02ac95ddcf6b
Siyeon <gkatldus1@gmail.com>
2021-07-26 07:49:02 +0900

    ps-solve

--- baek11057.py
+++ baek11057.py
@@ -1,10 +1,10 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-s = [[0]*10 for i in range(1001)]
+s = [[0]*10 for i in range(n+1)]
 for i in range(10):
     s[1][i] = 1
-for i in range(2, 1001):
+for i in range(2, n+1):
     for j in range(10):
         for k in range(j+1):
             s[i][j] += s[i-1][k]
