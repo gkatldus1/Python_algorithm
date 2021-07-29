4763c5d488a7ddcd73af12eb4560b65cb2378b05
gkatldus1 <gkatldus1@gmail.com>
2021-07-29 11:15:28 +0900

    ps-solve

---
+++ baek2156.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+w = [0] * 10000
+n = int(input())
+for i in range(n):
+    w[i+1] = int(input())
+dp = [0] * 10000
+dp[1] = w[1]
+if n > 1:
+    dp[2] = w[1] + w[2]
+for i in range(3, n+1):
+    dp[i] = (max(dp[i-1], dp[i-2]+w[i], dp[i-3]+w[i-1]+w[i]))
+print(dp[n])
