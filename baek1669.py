3a69176a35101cdd9da282b43ecb3b6bb9eefbd3
Siyeon <gkatldus1@gmail.com>
2021-08-19 06:47:56 +0900

    ps-solve

--- baek1669.py
+++ baek1669.py
@@ -1,11 +1,15 @@
-n = int(input())
-dp = [0 for i in range(n+1)]
-square = [i*i for i in range(1, 317)]
-for i in range(1, n+1):
-    s = []
-    for j in square:
-        if j > i:
-            break
-        s.append(dp[i-j])
-    dp[i] = min(s) + 1
-print(dp[n])
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+if a == b:
+    print(0)
+else:
+    n = int((b - a) ** 0.5)
+    if n ** 2 == b - a:
+        print(2*n -1)
+    else:
+        z = (b-a) - n**2
+        if z <= n:
+            print(2 * n)
+        else:
+            print(2 * n + 1 )
