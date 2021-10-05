d630cacc20b6788b5bd623b877effed544f75515
Siyeon <gkatldus1@gmail.com>
2021-10-05 23:33:28 +0900

    ps-solve

---
+++ baek1934.py
@@ -0,0 +1,21 @@
+import sys
+import math
+input = sys.stdin.readline
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+n = int(input())
+
+for i in range(n):
+    a, b = map(int, input().split())
+    # print(math.lcm(a, b))
+    print(lcm(a, b))
