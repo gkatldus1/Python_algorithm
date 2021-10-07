8e123146a726549cbf1554c29ed0b3f2deb78b49
Siyeon <gkatldus1@gmail.com>
2021-10-07 13:58:35 +0900

    ps-solve

--- baek1850.py
+++ baek1850.py
@@ -2,7 +2,7 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-X, Y = (N, M) if N > M else(M, N)
+X, Y = (N, M) if N > M else (M, N)
 
 while X % Y:
     X, Y = Y, X % Y
---
+++ baek9613.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+import sys
+
+input = sys.stdin.readline
+
+# result = list(combinations(arr, 2))
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+t = int(input())
+
+for i in range(t):
+    final = []
+    arr = [element for element in map(int, input().split())]
+    arr = arr[1:]
+    result = list(combinations(arr, 2))
+    for j in result:
+        final.append(gcd(j[0], j[1]))
+    print(sum(final))
+
+    # print(sum(result))
