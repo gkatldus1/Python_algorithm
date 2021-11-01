20216d384fd7a344ae89a40389b0dfa2f74b1d08
Siyeon <gkatldus1@gmail.com>
2021-11-01 20:43:53 +0900

    ps-solve

--- baek6588.py
+++ baek6588.py
@@ -1,21 +1,48 @@
+# import sys
+# input = sys.stdin.readline
+# r = 1000000
+# primes = [True for _ in range(1000000)]
+# for i in range(2, int(r**0.6)):
+#     if primes[i] == True:
+#         for j in range(i*2, r, i):
+#             if primes[j] == True:
+#                 primes[j] = False
+
+# while(True):
+#     n = int(input())
+
+#     if n == 0:
+#         break
+#     for i in range(3, r):
+#         if primes[i] == True:
+#             if primes[n-i] == True:
+#                 print("%d = %d + %d"%(n, i, n-i))
+#                 break
 import sys
-input = sys.stdin.readline
-r = 1000000
-primes = [True for _ in range(1000000)]
-for i in range(2, int(r**0.6)):
-    if primes[i] == True:
-        for j in range(i*2, r, i):
-            if primes[j] == True:
-                primes[j] = False
 
-while(True):
-    n = int(input())
+def Goldbach():
+    check = [False, False] + [True] * 1000000
 
-    if n == 0:
-        break
-    for i in range(3, r):
-        if primes[i] == True:
-            if primes[n-i] == True:
-                print("%d = %d + %d"%(n, i, n-i))
+    for i in range(2, 1001):
+        if check[i] == True:
+            for j in range(i + i, 1000001, i):
+                check[j] = False
+    
+    while True:
+        n = int(sys.stdin.readline())
+
+        if n == 0:
+            break
+
+        A = 0
+        B = n
+        for _ in range(1000000):
+            if check[A] and check[B]:
+                print(f"{n} = {A} + {B}")
                 break
+            A += 1
+            B -= 1
+        else:
+            print("Goldbach's conjecture is wrong")
 
+Goldbach()
