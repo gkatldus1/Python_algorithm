36abe0a7c03188d2a1a491c7625af343e0049d28
gkatldus1 <gkatldus1@gmail.com>
2022-01-31 14:00:39 +0900

    ps-solve

---
+++ leet332.py
@@ -0,0 +1,46 @@
+import collections
+def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+    graph = collections.defaultdict(list)
+
+    for a, b in sorted(tickets):
+        graph[a].append(b)
+
+    route = []
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop(0))
+        route.append(a)
+
+    dfs('JFK')
+
+    return route[::-1]
+
+def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+    graph = collections.defaultdict(list)
+
+    for a, b in sorted(tickets, reverse=True):
+        graph[a].append(b)
+
+    route = []
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop())
+        route.append(a)
+
+    dfs('JFK')
+
+    return route[::-1]
+
+def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+    graph = collections.defaultdict(list)
+
+    for a, b in sorted(tickets):
+        graph[a].append(b)
+    
+    route, stack = [], ['JFK']
+    while stack:
+        while graph[stack[-1]]:
+            stack.append(graph[stack[-1]].pop(0))
+        route.append(stack.pop())
+    
+    return route[::-1]
