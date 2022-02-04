88e193dee0552dad60f784ac28db4c231d3e5f75
Siyeon <gkatldus1@gmail.com>
2022-02-04 20:40:40 +0900

    ps-solve

--- leet787.py
+++ leet787.py
@@ -7,7 +7,7 @@ def findCheapestPrice(self, n: int, flights: List[List[int]],src: int, dst: int,
     for u, v, w in flights:
         graph[u].append((v, w))
 
-        Q = [(0, src, K)]
+    Q = [(0, src, K)]
 
     while Q:
         price, node, k = heapq.heappop(Q)
