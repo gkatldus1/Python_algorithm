415a3715547171c10646292dde6459eb683c8641
gkatldus1 <gkatldus1@gmail.com>
2022-02-14 21:47:51 +0900

    ps-solve

---
+++ leet105.py
@@ -0,0 +1,9 @@
+def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+    if inorder:
+        index = inorder.index(preorder.pop(0))
+
+        node = TreeNode(inorder[index])
+        node.left = self.buildTree(preorder, inorder[0:index])
+        node.right = self.buildTree(preorder, inorder[index+1:])
+
+        return node
