8a2a4f65a9709e39c2fe8468773e0416b73d02b0
gkatldus1 <gkatldus1@gmail.com>
2022-02-14 12:24:13 +0900

    ps-solve

---
+++ leet783.py
@@ -0,0 +1,17 @@
+import sys
+
+class Solution:
+    prev = -sys.maxsize
+    result = sys.maxsize
+
+    def minDiffInBST(self, root: TreeNode) -> int:
+        if root.left:
+            self.minDiffInBST(root.left)
+
+        self.result = min(self.result, root.val - self.prev)
+        self.prev = root.val
+
+        if root.right:
+            self.minDiffInBST(root.right)
+
+        return self.result
