4f6d79969069c27a7a93bc5803ada157be44eacf
Siyeon <gkatldus1@gmail.com>
2022-03-03 10:07:43 +0900

    ps-solve

---
+++ leet147.py
@@ -0,0 +1,22 @@
+def insertionSortList(self, head: ListNode) -> ListNode:
+    cur = parent = ListNode(None)
+    while head:
+        while cur.next and cur.next.val < head.val:
+            cur = cur.next
+
+        cur.next, head.next, head = head, cur.next, head.next
+
+        cur = parent
+    return cur.next
+
+def insertionSortList(self, head: ListNode) -> ListNode:
+    cur = parent = ListNode(0)
+    while head:
+        while cur.next and cur.next.val < head.val:
+            cur = cur.next
+        
+        cur.next, head.next, head = head, cur.next, head.next
+
+        if head and cur.val > head.val:
+            cur = parent
+    return parent.next
--- leet56.py
+++ leet56.py
@@ -4,5 +4,5 @@ def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if merged and i[0] <= merged[-1][1]:
             merged[-1][1] = max(merged[-1][1], i[1])
         else:
-            merged += i
+            merged += i,
     return merged
