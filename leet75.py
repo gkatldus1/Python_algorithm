a00a4ab54f0e71b158ace44f20928d2a3471eede
Siyeon <gkatldus1@gmail.com>
2022-03-05 12:46:48 +0900

    ps-solve

---
+++ leet75.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def sortColors(self, nums: List[int]) -> None:
+    red, white, blue = 0, 0, len(nums)
+
+    while white < blue:
+        if nums[white] < 1:
+            nums[red], nums[white] = nums[white], nums[red]
+            white += 1
+            red += 1
+        elif nums[white] > 1:
+            blue -= 1
+            nums[blue], nums[white] = nums[white], nums[blue]
+        else:
+            white += 1
