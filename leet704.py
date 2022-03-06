7922feaeb6e9f24dd34f6570122f1d38a95b8180
Siyeon <gkatldus1@gmail.com>
2022-03-06 15:45:23 +0900

    ps-solve

--- leet704.py
+++ leet704.py
@@ -1,4 +1,5 @@
 from typing import List
+import bisect
 
 def search(self, nums: List[int], target: int) -> int:
     def binary_search(left, right):
@@ -17,4 +18,30 @@ def search(self, nums: List[int], target: int) -> int:
 
     return binary_search(0, len(nums) - 1)
 
+def search(self, nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
 
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+    
+    return -1
+
+def search(self, nums: List[int], target: int) -> int:
+    index = bisect.bisect_left(nums, target)
+
+    if index < len(nums) and nums[index] == target:
+        return index
+    else:
+        return -1
+
+def search(self, nums: List[int], target: int) -> int:
+    try:
+        return nums.index(target)
+    except ValueError:
+        return -1
