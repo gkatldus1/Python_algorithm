a571027a62df3f1ec940b22467a07f8317d04907
Siyeon <gkatldus1@gmail.com>
2022-03-26 13:43:38 +0900

    ps-solve

---
+++ 18kakao04.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def solution(n: int, t: int, m: int, timetable: List[str]) -> str:
+    timetable = [
+        int(time[:2]) * 60 + int(time[3:])
+        for time in timetable
+    ]
+    timetable.sort()
+
+    current = 540
+
+    for _ in range(n):
+        for _ in range(m):
+            if timetable and timetable[0] <= current:
+                candidate = timetable.pop[0] <= current:
+            else:
+                candidate = current
+        current += t
+    h, m = divmod(candidate, 60)
+    return str(h).zfill(2) + ':' + str(m).zfill(2)
