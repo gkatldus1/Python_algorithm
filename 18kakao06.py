6af56ab0bc4db8b84a700712744285c4a6841838
Siyeon <gkatldus1@gmail.com>
2022-03-29 10:09:32 +0900

    ps-solve

---
+++ 18kakao06.py
@@ -0,0 +1,24 @@
+from typing import List
+
+def solution(m: int, n: int, board: List[str]) -> int:
+    board = [list(x) for x in board]
+
+    matched = True
+    while matched:
+        mathched = []
+        for i in range(m - 1):
+            for j in range(n - 1):
+                if board[i][j] == board[i][j + 1] == board[i + 1][j + 1] == board[i + 1][j] != '#':
+                    matched.append([i,j])
+        
+        for i, j in matched:
+            board[i][j] = board[i][j + 1] = board[i + 1][j + 1] = board[i + 1][j] = '#'
+
+        for _ in range(m):
+            for i in range(m - 1):
+                for j in range(n):
+                    if board[i + 1][j] == '#':
+                        board[i + 1][j], board[i][j] = board[i][j], '#'
+
+    return sum(x.count('#') for x in board)
+
