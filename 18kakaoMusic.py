58cedbc8aae421e574639b89d9ebc41ae74cb79b
Siyeon <gkatldus1@gmail.com>
2022-04-07 15:28:00 +0900

    ps-solve

---
+++ 18kakaoMusic.py
@@ -0,0 +1,25 @@
+import datetime
+def solution(m, musicinfos):
+    answer = ''
+    candidate = []
+    m = m.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+    for music in musicinfos:
+        music = music.split(",")
+        music[3] = music[3].replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a")
+        start = datetime.datetime.strptime(music[0], "%M:%S").timestamp()
+        end = datetime.datetime.strptime(music[1], "%M:%S").timestamp()
+        play = int(end - start)
+        l, n = divmod(play, len(music[3]))
+        # print(m, n)
+        sound = music[3]*l +"".join([music[3][i] for i in range(n) ])
+        print(m, sound)
+        if m not in sound:
+            continue
+    
+        if answer == None or play > answer[1]:
+                answer = (music[2], play)
+                
+    if answer:
+        return answer[0]
+                        
+    return None
