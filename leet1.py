f1e820ab571aae1d45c2502de10c23f1a663fe0d
Siyeon <gkatldus1@gmail.com>
2022-04-11 13:28:20 +0900

    ps-solve

--- leet1.py
+++ leet1.py
@@ -1,4 +1,3 @@
-from tkinter import N
 from typing import List
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -14,4 +13,37 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
 
             if complement in nums[i + 1:]:
                 return [i, nums[i + 1:].index(complement) + (i + 1)]          
+        
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+
+    for i, num in enumerate(nums):
+        nums_map[num] = i
+
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            return [i, nums_map[target - num]]
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            return [nums_map[target - num], i]
+        
+        nums_map[num] = i
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    left, right = 0, len(nums) - 1
+    while not left == right:
+        sum_ = nums[right] + nums[left]
+
+        if sum_ < target:
+            left += 1
+        elif sum_ > target:
+            right -= 1
+        else:
+            return left, right
+
+
         
