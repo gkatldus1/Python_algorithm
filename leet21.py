377114d3c7c1f87548d0f55f48277a76aaae4f33
Siyeon <gkatldus1@gmail.com>
2022-04-18 11:45:18 +0900

    ps-solve

---
+++ leet21.py
@@ -0,0 +1,33 @@
+from typing import *
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+
+
+
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = cur = ListNode(0)
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        cur.next = list1 or list2
+        return dummy.next
+
+
---
+++ leet234.py
@@ -0,0 +1,49 @@
+from typing import ListNode, Optional, List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        values = []
+        while head != None:
+            values.append(head.val)
+            head = head.next
+        if values == values[::-1]:
+            return True
+        else:
+            return False
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        q: List = []
+
+        if not head:
+            return True
+        
+        node = head
+
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+        
+        return True
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
