bd1c4f852f6809f828d07de62e615bb558c488b3
Siyeon <gkatldus1@gmail.com>
2022-04-21 13:49:05 +0900

    ps-solve

--- leet2.py
+++ leet2.py
@@ -7,32 +7,36 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l1_num = 0
-        l2_num = 0
+        l1_num, l2_num = 0, 0
         decimal = 0
-        l1_node = l1
-        l2_node = l2
+        l1_node, l2_node = l1, l2
 
-        while l1:
+        while l1_node:
             l1_num += l1_node.val * (10 ** decimal)
-            l2_num += l2_node.val * (10 ** decimal)
-            l1 = l1_node.next
-            l2 = l2_node.next
+            l1_node = l1_node.next
             decimal += 1
-        decimal -= 1
-        l1_num += l2_num
-        ans_node = ListNode()
-        temp = 0
-        while not (decimal < 0):
-            temp = l1.val // (10 ** decimal)
-            l1.val = l1.val % (10 ** decimal)
-            node = ListNode(temp)
-            node.next = ans_node
-            ans_node = node
-            decimal -= 1
+        decimal = 0
+        while l2_node:
+            l2_num += l2_node.val * (10 ** decimal)
+            l2_node = l2_node.next
+            decimal += 1
             
-        return ans_node
+        l1_num += l2_num
+        print(l1_num)
+        num_string = str(l1_num)
+        
+        prev: ListNode = None
+        for r in num_string:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
 
+        return node
+
+
+
+
+# ----------------------------------------------------------------------------
 
     def reverseList(self, head: ListNode) -> ListNode:
         node, prev = head, None
