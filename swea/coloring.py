e091ebec250d94b0859d00022b3238e8964f8a01
Siyeon <gkatldus1@gmail.com>
2022-05-06 13:33:44 +0900

    ps-solve

--- swea/coloring.py
+++ swea/coloring.py
@@ -1,7 +1,7 @@
 T = int(input())
 
 for loop in range(T):
-    _map = [[0 for i in range(10)] for i in range(10)]
+    _map = [[0]*10 for _ in range(10)]
     N = int(input())
     count = 0
     for i in range(N):
@@ -13,6 +13,24 @@ for loop in range(T):
                     count += 1
                 else:
                     _map[j][k] = info[4]
-        print("#%d %d"%(loop+1, count))
+    print("#%d %d"%(loop+1, count))
+
+
+T = int(input())
+
+for loop in range(T):
+    _map = [[0]*10 for _ in range(10)]
+    N = int(input())
+    count = 0
+    for i in range(N):
+        x1, y1, x2, y2, color = map(int, input.split())
+
+        for j in range(x1,x2 + 1):
+            for k in range(y1, y2 + 1):
+                if _map[j][k] != 0:
+                    count += 1
+                else:
+                    _map[j][k] = color
+    print("#%d %d"%(loop+1, count))
 
 
