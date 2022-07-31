6842256733bcb65159f7c248bc593777dca086be
Siyeon <gkatldus1@gmail.com>
2022-07-31 10:47:58 +0900

    ps-solving

---
+++ java_solve/Baek11003.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Baek11003 {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
